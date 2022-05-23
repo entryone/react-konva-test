4d69820a381c1218556c8541907bb1fa47886e35
entryone <a.i.kulinich@gmail.com>
2022-05-23 13:51:11 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,31 +3,31 @@ import "./style.css";
 import { Stage, Layer, Rect, Text } from "react-konva";
 
 
-function ColoredRect () {
-
-
-  
-  
-    return (
+function Cell ({textAlign = 'left', text = '', x = 1, y = 1, width = 50, height = 27, fill='white'}) {
+    return (<>
       <Rect
-        x={20}
-        y={20}
-        width={50}
-        height={50}
-        fill={'red'}
-        shadowBlur={5}
-        onClick={() => {}}
+        
+        stroke={'green'}
+        strokeWidth={0.5}
+        x={x}
+        y={y}
+        width={width}
+        height={height}
+        fill={fill}
+        shadowEnabled={false}
       />
-    );
+      <Text align={textAlign} x={x} y={y + 5} tooltip='fff' text={text} width={width} wrap={'none'} ellipsis  />
+    </>);
   
 }
 
 export default function App() {
   return (
     <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-          <Text text="Try click on rect" width={40} />
-          <ColoredRect />
+        <Layer onWheel={() => {console.log('scroll')}} >
+        <Cell text={'sdf sdf sdf sdf'} fill={'transparent'} />
+        <Cell textAlign={'right'} y={28} width={200} text={'12345678903qewert'} fill={'transparent'} />
+        
         </Layer>
       </Stage>
   );
